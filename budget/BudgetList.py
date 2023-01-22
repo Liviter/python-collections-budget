e257ea6984529247d6844e3f7663da9980b59c58
Liviter <77466244+Liviter@users.noreply.github.com>
2023-01-22 22:54:18 +0000

    commit 1 module 2

---
+++ budget/BudgetList.py
@@ -0,0 +1,18 @@
+class BudgetList():
+    def __init__(self, budget):
+        self.budget = budget
+        self.sum_expenses = 0
+        self.expenses = []
+        self.sum_overages = 0
+        self.overages = []
+
+    def append(self, item):
+        if self.sum_expenses + item < self.budget:
+            self.expenses.append(item)
+            self.sum_expenses += item
+
+    def __len__(self):
+        return len(self.expenses + self.overages)
+
+    #def main(self):
+    #    myBudgetList = BudgetList(1200)

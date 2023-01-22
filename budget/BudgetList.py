e82d0a4aaf8b4f969d916988427bd0f6ffdd3cb4
Liviter <77466244+Liviter@users.noreply.github.com>
2023-01-22 23:08:24 +0000

    finished module 2

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,3 +1,6 @@
+from . import Expense
+
+
 class BudgetList():
     def __init__(self, budget):
         self.budget = budget
@@ -10,9 +13,23 @@ class BudgetList():
         if self.sum_expenses + item < self.budget:
             self.expenses.append(item)
             self.sum_expenses += item
+        else:
+            self.overages.append(item)
+            self.sum_overages += item
+
 
     def __len__(self):
         return len(self.expenses + self.overages)
 
-    #def main(self):
-    #    myBudgetList = BudgetList(1200)
+def main():
+    myBudgetList = BudgetList(1200)
+    expenses = Expense.Expenses()
+    expenses.read_expenses('data/spending_data.csv')
+
+    for expense in expenses.list:
+        myBudgetList.append(expense.amount)
+
+    print('The count of all expenses: ' + str(len(myBudgetList)))
+
+if __name__ == "__main__":
+    main()
